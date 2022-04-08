9222875629d37de1c6e598a2e2fd8c05f5f60a1c
Saad Tariq <saad.tarik@outlook.com>
2022-04-08 16:20:18 +0100

    initialising git again after issues

---
+++ front-end/src/components/Copyright.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Copyright = () => {
+  return (
+    <div className="copyright">
+      <p className="p-text">@2020 MICHAEL</p>
+      <p className="p-text">All rights reserved</p>
+    </div>
+  );
+};
+
+export default Copyright;
